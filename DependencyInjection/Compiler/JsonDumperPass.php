5be3165ce7942557948ee8a21683a4d8876c1faf
raph <raph@e-frogg.com>
2022-01-25 21:02:45 +0100

    - service cms.storyblok_node_provider_json_dumper in services.yaml

--- DependencyInjection/Compiler/JsonDumperPass.php
+++ DependencyInjection/Compiler/JsonDumperPass.php
@@ -2,9 +2,7 @@
 
 namespace Efrogg\Bundle\StoryblokBundle\DependencyInjection\Compiler;
 
-use Efrogg\Bundle\StoryblokBundle\Log\StoryblokLogger;
 use Efrogg\ContentRenderer\Core\Resolver\ContainerTag;
-use Efrogg\ContentRenderer\NodeProvider\JsonDumperNodeProvider;
 use Efrogg\ContentRenderer\NodeProvider\SimpleJsonFileNodeProvider;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -24,16 +22,15 @@ class JsonDumperPass implements CompilerPassInterface
 
         if (($dumperConfiguration['write'] ?? false)) {
             // remove original node provider tag
-            $definition = $container->getDefinition('cms.storyblok_node_provider')
-                                    ->clearTag(ContainerTag::TAG_NODE_PROVIDER);
+            $container
+                ->getDefinition('cms.storyblok_node_provider')
+                ->clearTag(ContainerTag::TAG_NODE_PROVIDER);
 //
             // inject new service instead
-            $container->register('cms.storyblok_node_provider_json_dumper', JsonDumperNodeProvider::class)
-                      ->addArgument($dumperConfiguration['dump-path'])
-                      ->addMethodCall('setNodeProvider', [$definition])
-                      ->addMethodCall('setLogger', [new Reference('cms.storyblok.logger')])
-//             ->addMethodCall('addConverterDecorator', [new UTF8EncodeStringDecorator()])
-                      ->addTag(ContainerTag::TAG_NODE_PROVIDER);
+            $container
+                ->getDefinition('cms.storyblok_node_provider_json_dumper')
+                ->addTag(ContainerTag::TAG_NODE_PROVIDER);
+
         }
 
         if (($dumperConfiguration['read'] ?? false)) {
