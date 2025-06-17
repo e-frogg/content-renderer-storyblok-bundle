ab2d50468ac757f94c61a7682e7a5b2665db0e67
raph <raph@e-frogg.com>
2025-06-17 15:43:26 +0200

    fix deprecations

--- DependencyInjection/StoryblokExtension.php
+++ DependencyInjection/StoryblokExtension.php
@@ -5,8 +5,8 @@ namespace Efrogg\Bundle\StoryblokBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class StoryblokExtension extends Extension
 {
